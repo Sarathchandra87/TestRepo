545fd38b61ffa0eebf5c8eeb02db079f4b09b5e7
Sarathchandra <sarathchandrab4u@gmail.com>
2013-01-31 15:08:31 -0600

    Initial Commit

---
+++ MySourceControlApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySourceControlApp
+//
+//  Created by L&T Infotech on 1/31/13.
+//  Copyright (c) 2013 L&T Infotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
